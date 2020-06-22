6eee06640719c5f4cac408371f51e05d626a7999
amale <aidrissi001@gmail.com>
2020-06-22 10:09:57 +0200

    Javadoc

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -6,6 +6,9 @@ import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.helpers.HelperClass;
 import com.parkit.parkingsystem.model.Ticket;
 
+/**
+ * The system calculates fare based on the parking time and the vehicule type (car/bike).
+ */
 public class FareCalculatorService {
     private static HelperClass helper = new HelperClass();
     private static TicketDAO ticketDAO = new TicketDAO();
@@ -14,6 +17,12 @@ public class FareCalculatorService {
         this.ticketDAO = ticketDAO;
     }
 
+    /**
+     * Calculation of the parking fees.
+     *
+     * @param ticket, the ticket with the parking informations of the user.
+     * @throws IllegalArgumentException, if the out time provided is incorrect.
+     */
     public void calculateFare(Ticket ticket) {
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
             throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
@@ -35,6 +44,12 @@ public class FareCalculatorService {
         }
     }
 
+    /**
+     * Calculation of the parking fees with discount.
+     * @param parkingType, it refers to the vehicule type (car/bike).
+     * @param duration, parking time. It's the elapsed time, given in hours, between parking entry and exit.
+     * @return the price calculated based on the parking time, the vehicule type and the 5% discount.
+     */
     private double getPriceWithDiscount(ParkingType parkingType, double duration) {
         switch (parkingType) {
             case CAR: {
@@ -48,6 +63,12 @@ public class FareCalculatorService {
         }
     }
 
+    /**
+     * Calculation of the parking fees without discount.
+     * @param parkingType, it refers to the vehicule type (car/bike).
+     * @param duration, parking time. It's the elapsed time, given in hours, between parking entry and exit.
+     * @return the price calculated based on the parking time and the vehicule type.
+     */
     private double getPriceWithoutDiscount(ParkingType parkingType, double duration) {
         switch (parkingType) {
             case CAR: {
@@ -61,6 +82,11 @@ public class FareCalculatorService {
         }
     }
 
+    /**
+     * Free fees for parking under thirty minutes.
+     * @param duration, parking time. It's the elapsed time, given in hours, between parking entry and exit.
+     * @return free parking fees under thirty minutes.
+     */
     private double getPriceFree30MinParking(double duration) {
         return duration * Fare.UNDER_THIRTY_MINUTES;
     }
