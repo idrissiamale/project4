3da50a0a07908b6dea983387ea1db155fd5bf23b
amale <aidrissi001@gmail.com>
2020-06-23 11:32:13 +0200

    Documentation of the HelperClass

--- src/main/java/com/parkit/parkingsystem/helpers/HelperClass.java
+++ src/main/java/com/parkit/parkingsystem/helpers/HelperClass.java
@@ -2,6 +2,9 @@ package com.parkit.parkingsystem.helpers;
 
 import java.math.BigDecimal;
 
+/**
+ * A class whose methods are used to convert parking time into hours and to round fees to three decimal places.
+ */
 public class HelperClass {
     /**
      * Helper method which permits to convert the elapsed time into minutes.
@@ -9,6 +12,7 @@ public class HelperClass {
      * @param end,   it refers to the ending time.
      * @param start, it refers to the starting time.
      * @return durationInMinutes which is the difference between the ending time and the starting time in minutes.
+     * @see com.parkit.parkingsystem.service.FareCalculatorService
      */
     public static double getMinutes(double end, double start) {
         double durationInMinutes = (end - start) / 60000;
@@ -20,6 +24,7 @@ public class HelperClass {
      *
      * @param minutes, it refers to the time in minutes.
      * @return hours, it returns the time in hours.
+     * @see com.parkit.parkingsystem.service.FareCalculatorService
      */
     public static double getHours(double minutes) {
         double hours = minutes / 60;
@@ -31,6 +36,7 @@ public class HelperClass {
      *
      * @param n, the number to round.
      * @return n, it returns the number rounded to three decimal places.
+     * @see com.parkit.parkingsystem.service.FareCalculatorService
      */
     public static double getTheRoundToThreeDecimalPlaces(double n) {
         BigDecimal bd = new BigDecimal(n);
