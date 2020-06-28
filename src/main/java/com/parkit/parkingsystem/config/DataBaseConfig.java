0707a898b4d52f25e6df899e94d2d3c4cadf945a
amale <aidrissi001@gmail.com>
2020-06-28 19:55:44 +0200

    creation of the properties file "database.properties" with DB driver, url, user and password. Loading it in DataBaseConfig and DataBaseTestConfig classes to establish a connection to the Prod or Test db

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -3,7 +3,11 @@ package com.parkit.parkingsystem.config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * A class which configures and gives access to the parking system database.
@@ -18,12 +22,22 @@ public class DataBaseConfig {
      * @return a connection to the url.
      * @throws ClassNotFoundException will be thrown if the JDBC driver is not available in Classpath or if the class loader is not able to find it.
      * @throws SQLException           will be thrown if database access errors occurs or url is null.
+     * @throws IOException            Loading the properties file "database.properties" with the JDBC driver, url, username and password.
+     *                                IOException will be thrown if the file is not found.
      */
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException, IOException {
+        Properties properties = new Properties();
+        FileInputStream in = new FileInputStream("src/main/resources/database.properties");
+        properties.load(in);
+        in.close();
+        String driver = properties.getProperty("jdbc.driver");
+        String url = properties.getProperty("jdbc.url");
+        String username = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
         logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName(driver);
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "rootroot");
+                url, username, password);
     }
 
     /**
--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -4,7 +4,10 @@ import com.parkit.parkingsystem.config.DataBaseConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * A test class version of DataBaseConfig class which gives access to the test database.
@@ -21,12 +24,22 @@ public class DataBaseTestConfig extends DataBaseConfig {
      * @return a connection to the url.
      * @throws ClassNotFoundException will be thrown if the JDBC driver is not available in Classpath or if the class loader is not able to find it.
      * @throws SQLException           will be thrown if database access errors occurs or url is null.
+     * @throws IOException            Loading the properties file "database.properties" with the JDBC driver, url, username and password.
+     *                                IOException will be thrown if the file is not found.
      */
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException, IOException {
+        Properties properties = new Properties();
+        FileInputStream in = new FileInputStream("src/main/resources/database.properties");
+        properties.load(in);
+        in.close();
+        String driver = properties.getProperty("jdbc.driver");
+        String url = properties.getProperty("jdbc.url");
+        String username = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
         logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName(driver);
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "rootroot");
+                url, username, password);
     }
 
     /**
